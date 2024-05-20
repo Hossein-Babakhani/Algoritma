1bf68d6576281ecf31a48c3659ab39cab61904ca
Hossein Babakhani <bna.babakhani@gmail.com>
2024-05-20 20:58:00 +0300

    Add files via upload

---
+++ Algoritma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir dize girin: ");
+            string str = Console.ReadLine();
+
+            Console.Write("Bir dizin girin:");
+            int dizin = Convert.ToInt32(Console.ReadLine());
+            
+            if (dizin<0 || dizin>=str.Length)
+            {
+                Console.Write("Geçersiz dizin");
+                return;
+            }
+            string newStr = str.Substring(0, dizin) + str.Substring(dizin + 1);
+            Console.WriteLine("Sonuç:" + newStr);
+            Console.ReadKey();
+        }
+    }
+}
